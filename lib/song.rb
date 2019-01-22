c826e76fcc41d1eec8f381cf4a8bfc7a187a3658
Aglae Verduzco <aglae.cv@gmail.com>
2019-01-22 02:21:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,4 +26,14 @@ class Song
   def Song.genres
     @@genres.uniq
   end
+
+  def Song.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+    if genre_count[genre]
+    genre_count[genre] +=1
+    end  
+  end
+    genre_count
+  end
 end
