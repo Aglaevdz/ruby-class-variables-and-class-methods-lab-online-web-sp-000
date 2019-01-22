b4e97d3e1bcd92e6042c1d6934ab1f244f4d4513
Aglae Verduzco <aglae.cv@gmail.com>
2019-01-22 02:02:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,4 +15,16 @@ class Song
     @@song_count +=1
   end
 
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
 end
